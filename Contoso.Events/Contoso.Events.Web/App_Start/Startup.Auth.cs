4e0ee137681c4eb7fd73a38ed8dadcd8d51309b2
DEVOBS2017 <mark.halftrip@gmail.com>
2017-06-06 13:40:22 +0300

    k

--- Contoso.Events/Contoso.Events.Web/App_Start/Startup.Auth.cs
+++ Contoso.Events/Contoso.Events.Web/App_Start/Startup.Auth.cs
@@ -34,7 +34,7 @@ namespace Contoso.Events.Web
                 {
                     ClientId = clientId,
                     Authority = authority,
-                    PostLogoutRedirectUri = postLogoutRedirectUri
+                    PostLogoutRedirectUri = kekkonen-kekkonen.azurewebsites.net
                 });
         }
     }
